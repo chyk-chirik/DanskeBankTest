04d2cd88e5c646b619b173ec2e269dde41889cd7
Anton Putau <putovanton@outlook.com>
2026-04-15 19:04:58 +0300

    add facade rate service test and implementation

---
+++ DanskeBankTest.Services.Tests/ExchangeRateServiceTests.cs
@@ -0,0 +1,82 @@
+﻿using DanskeBankTest.Services.ExchangeRate;
+using DanskeBankTest.Services.Types;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DanskeBankTest.Services.Tests
+{
+    [TestClass]
+
+    public sealed class ExchangeRateServiceTests
+    {
+        private CurrencyRate SuccessCurrencyRate1 = new CurrencyRate(new CurrencyPair(Currency.Usd, Currency.Eur), 1m);
+        private CurrencyRate SuccessCurrencyRate2 = new CurrencyRate(new CurrencyPair(Currency.Usd, Currency.Gbp), 2m);
+       
+        private IExchangeRateService alwaysSuccessRateService1;
+        private IExchangeRateService alwaysSuccessRateService2;
+        private IExchangeRateService alwaysErrorRateService;
+
+        public ExchangeRateServiceTests()
+        {
+            var successRateServiceMock1 = new Mock<IExchangeRateService>();
+            successRateServiceMock1
+                .Setup(s => s.GetExchangeRate(It.IsAny<CurrencyPair>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(SuccessCurrencyRate1);
+
+            var successRateServiceMock2 = new Mock<IExchangeRateService>();
+            successRateServiceMock2
+                .Setup(s => s.GetExchangeRate(It.IsAny<CurrencyPair>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(SuccessCurrencyRate2);
+
+            var alwaysErrorRateServiceMock = new Mock<IExchangeRateService>();
+            alwaysErrorRateServiceMock
+                .Setup(s => s.GetExchangeRate(It.IsAny<CurrencyPair>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new Exception("Error"));
+
+            alwaysSuccessRateService1 = successRateServiceMock1.Object;
+            alwaysSuccessRateService2 = successRateServiceMock2.Object;
+            alwaysErrorRateService = alwaysErrorRateServiceMock.Object;
+        }
+
+        [TestMethod]
+        public async Task RealtimeServiceException_ReturnRateFromOfflineService()
+        {
+            var facadeRateService = new ExchangeRateFacadeService(
+                realtimeRateService: alwaysErrorRateService,
+                offlineRateService: alwaysSuccessRateService1);
+
+            var anyPair = new CurrencyPair(Currency.Usd, Currency.Eur);
+            var rate = await facadeRateService.GetExchangeRate(anyPair, CancellationToken.None);
+
+            rate.ShouldBe(SuccessCurrencyRate1);
+        }
+
+        [TestMethod]
+        public async Task RealtimeServiceHasPriorityOverOfflineService()
+        {
+            var facadeRateService = new ExchangeRateFacadeService(
+                realtimeRateService: alwaysSuccessRateService1,
+                offlineRateService: alwaysSuccessRateService2);
+
+            var anyPair = new CurrencyPair(Currency.Usd, Currency.Eur);
+            var rate = await facadeRateService.GetExchangeRate(anyPair, CancellationToken.None);
+
+            rate.ShouldBe(SuccessCurrencyRate1);
+        }
+
+        [TestMethod] // we highlight here assumption that inability to get rate is exceptional non business case
+        public async Task RealTimeAndOfflineServicesThrowExceptin_ThrowException()
+        {
+            var facadeRateService = new ExchangeRateFacadeService(
+                realtimeRateService: alwaysErrorRateService,
+                offlineRateService: alwaysErrorRateService);
+
+            var anyPair = new CurrencyPair(Currency.Usd, Currency.Eur);
+
+            Should.Throw<Exception>(async () => await facadeRateService.GetExchangeRate(anyPair, CancellationToken.None));
+        }
+    }
+}
---
+++ DanskeBankTest.Services/ExchangeRate/ExchangeRateFacadeService.cs
@@ -0,0 +1,26 @@
+﻿using DanskeBankTest.Services.Types;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DanskeBankTest.Services.ExchangeRate
+{
+    public class ExchangeRateFacadeService(
+        [FromKeyedServices(KeyedServicesNames.ExchangeRateRealtime)] IExchangeRateService realtimeRateService,
+        [FromKeyedServices(KeyedServicesNames.ExchangeRateOffline)] IExchangeRateService offlineRateService)
+        : IExchangeRateService
+    {
+        public async ValueTask<CurrencyRate> GetExchangeRate(CurrencyPair currencyPair, CancellationToken ct)
+        {
+            try
+            {
+                return await realtimeRateService.GetExchangeRate(currencyPair, ct);
+            }
+            catch (Exception ex)
+            {
+                return await offlineRateService.GetExchangeRate(currencyPair, ct);
+            }
+        }
+    }
+}
